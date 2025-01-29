0573fd37dcbfa5b84404addcda8284a0f086ed99
人皇帝辛 <120008435@qq.com>
2025-01-30 00:12:04 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -34,9 +34,9 @@ echo " ————————————————————————�
 # 删除旧域名
 cd && devil www del "$DOMAIN" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    echo " [OK] 默认域名 删除成功 "
+    echo "  默认域名 删除成功 "
 else
-    echo " [NO] 默认域名 删除失败 或 不存在"
+    echo "  默认域名 删除失败 或 不存在"
 fi
 
 # 删除旧目录
@@ -46,9 +46,9 @@ fi
 
 # 创建新域名
 if devil www add "$DOMAIN" nodejs /usr/local/bin/node22 > /dev/null 2>&1; then
-    echo " [OK] 类型域名 创建成功 "
+    echo "  类型域名 创建成功 "
 else
-    echo " [NO] 类型域名 创建失败，请检查环境设置 "
+    echo "  类型域名 创建失败，请检查环境设置 "
     exit 1
 fi
 
@@ -60,9 +60,9 @@ fi
 # 初始化 Node.js 环境
 cd "$DOMAIN_DIR" && npm init -y > /dev/null 2>&1
 if npm install dotenv basic-auth express axios > /dev/null 2>&1; then
-    echo " [OK] 环境依赖 安装成功 "
+    echo "  环境依赖 安装成功 "
 else
-    echo " [NO] 环境依赖 安装失败 "
+    echo "  环境依赖 安装失败 "
     exit 1
 fi
 
