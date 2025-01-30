ff0e3ad9708c482d362b07523bf3a56ee04ee2ef
人皇帝辛 <120008435@qq.com>
2025-01-30 11:46:52 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -813,18 +813,14 @@ app.get('/update', async (req, res) => {
                         data.forEach(update => {
                             let className = 'result-item';
                             if (update.success) {
-                                if (update.message.includes('删除')) {
-                                    className += ' failure';
-                                } else {
-                                    className += ' success';
-                                }
+                                className += ' success';
                             } else {
                                 className += ' failure';
                             }
                             resultHtml += \`
-                                <div class="\${className}">
-                                    <span>\${update.message}</span>
-                                </div>\`;
+                            <div class="\${className}">
+                                <span>\${update.message}</span>
+                            </div>\`;
                         });
 
                         document.getElementById('result').innerHTML = resultHtml;
