01170f558d31df3ebca017c6e0447d9b0dd8506f
人皇帝辛 <120008435@qq.com>
2025-02-11 19:49:17 +0800

    更新 install.sh

--- single/install.sh
+++ single/install.sh
@@ -28,7 +28,6 @@ A2="$A1/public_nodejs"
 B1="$A2/public"
 A3="https://github.com/ryty1/My-test/archive/refs/heads/main.zip"
 
-# 提示用户选择保活类型
 echo "请选择保活类型："
 echo "1. 本机保活"
 echo "2. 账号服务"
@@ -42,14 +41,13 @@ if [[ "$choice" -eq 1 ]]; then
 elif [[ "$choice" -eq 2 ]]; then
     TARGET_FOLDER="server"
     DELETE_FOLDER="single"
-    DEPENDENCIES="body-parser express-session session-file-store dotenv express socket.io node-cron node-telegram-bot-api axios"
+    DEPENDENCIES="body-parser express-session dotenv express socket.io node-cron node-telegram-bot-api axios"
     echo "开始进行 账号服务配置"
 else
     echo "无效选择，退出脚本"
     exit 1
 fi
 
-# 清理旧文件
 echo " ———————————————————————————————————————————————————————————— "
 cd && devil www del "$W" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
@@ -78,7 +76,6 @@ else
     exit 1
 fi
 
-# 下载并解压配置包
 wget "$A3" -O "$A2/main.zip" > /dev/null 2>&1
 if [[ $? -ne 0 || ! -s "$A2/main.zip" ]]; then
     X " 下载失败：文件不存在或为空" 1
@@ -87,34 +84,28 @@ else
     X " 下载 配置文件 " 0
 fi
 unzip -q "$A2/main.zip" -d "$A2" > /dev/null 2>&1
-B1="$A2/My-test-main"
+B1="$A2/serv00-save-me-main"
 if [[ -d "$B1" ]]; then
     mv "$B1"/* "$A2/"
     rm -rf "$B1"
 fi
-rm -f "$A2/README.md"
-rm -f "$A2/main.zip"
+rm -f "$A2/README.md" "$A2/main.zip"
 
-# 将选择的文件夹（single 或 server）中的文件前置到根目录，并删除另一个文件夹
 if [[ -d "$A2/$TARGET_FOLDER" ]]; then
-    # 复制选择的文件夹内容到根目录
     cp -r "$A2/$TARGET_FOLDER/." "$A2/"
     rm -rf "$A2/$TARGET_FOLDER"
 else
     exit 1
 fi
 
-# 删除未选择的文件夹
 if [[ -d "$A2/$DELETE_FOLDER" ]]; then
     rm -rf "$A2/$DELETE_FOLDER"
 fi
 
-# 设置本机保活相关文件的执行权限
 if [[ "$choice" -eq 1 ]]; then
+    rm -f "$A2/ota.sh" "$A2/install.sh"
     chmod 755 "$A2/app.js" > /dev/null 2>&1
     chmod 755 "$A2/hy2ip.sh" > /dev/null 2>&1
-    chmod 755 "$A2/install.sh" > /dev/null 2>&1
-    chmod 755 "$A2/ota.sh" > /dev/null 2>&1
 
     echo ""
     echo " 【 恭 喜 】： 本机保活  部署已完成  "
@@ -125,6 +116,7 @@ if [[ "$choice" -eq 1 ]]; then
     echo " ———————————————————————————————————————————————————————————— "
     echo ""
 else
+    rm -f "$A2/ota.sh"
     chmod 755 "$A2/app.js" > /dev/null 2>&1
     chmod 755 "$A2/ota.sh" > /dev/null 2>&1
 
